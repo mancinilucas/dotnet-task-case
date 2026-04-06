615791536cc8f079cc43162f815405c243c632b3
mancinilucas <mancini.lucasg@gmail.com>
2026-04-05 21:21:53 -0300

    feat: add Application TaskRepository interface

---
+++ TaskManagement.Application/Abstraction/ITaskRepository.cs
@@ -0,0 +1,8 @@
+﻿using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Application.Abstraction;
+
+public interface ITaskRepository
+{
+    Task AddAsync(TaskItem taskItem, CancellationToken cancellationToken = default);
+}
