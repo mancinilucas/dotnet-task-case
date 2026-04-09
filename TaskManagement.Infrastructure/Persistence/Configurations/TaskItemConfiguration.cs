a847efa0d9a1e86d67305cf69a1666d4ebdd3009
mancinilucas <mancini.lucasg@gmail.com>
2026-04-08 21:48:56 -0300

    feat: add TaskItemConfiguration

--- TaskManagement.Infrastructure/Persistence/Configurations/TaskItemConfiguration.cs
+++ TaskManagement.Infrastructure/Persistence/Configurations/TaskItemConfiguration.cs
@@ -1,6 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManagement.Domain.Entities;
+
 namespace TaskManagement.Infrastructure.Persistence.Configurations;
 
-public class TaskItemConfiguration
+public class TaskItemConfiguration : IEntityTypeConfiguration<TaskItem>
 {
+    public void Configure(EntityTypeBuilder<TaskItem> builder)
+    {
+        builder.ToTable("tasks");
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.Title)
+            .IsRequired()
+            .HasMaxLength(200);
+        
+        builder.Property(t => t.Description)
+            .IsRequired()
+            .HasMaxLength(1000);
+
+        builder.Property(t => t.Status)
+            .IsRequired();
+
+        builder.Property(t => t.AssignedUserId);
+        
+        builder.Property(t => t.CreatedAtUtc)
+            .IsRequired();
+    }
     
 }
