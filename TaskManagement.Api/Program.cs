7a78f701909f47ace76a5ade26cbd679243eb68f
mancinilucas <mancini.lucasg@gmail.com>
2026-04-12 13:56:42 -0300

    feat: add docker compose file, db connection and DI config

--- TaskManagement.Api/Program.cs
+++ TaskManagement.Api/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagement.Application.Abstraction;
+using TaskManagement.Application.UseCases.CreateTask;
+using TaskManagement.Infrastructure.Persistence;
+using TaskManagement.Infrastructure.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddScoped<CreateTaskUseCase>();
 
 var app = builder.Build();
 
